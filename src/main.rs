7608453028d95dcd4d4780b35ca3e5524675cd34
Will <willberrisford@gmail.com>
2025-12-14 21:35:59 +0000

    Initial ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use std::default;
+
+use ndarray::{Array3, iter};
+
 fn main() {
     println!("Hello, world!");
 }
 
-struct Board {}
+fn create_array(max_x: usize, max_y: usize, max_z: usize) -> Board {
+    let arr: Array3<Hex> = Array3::<Hex>::from_elem((max_x, max_y, max_z), Hex::default());
+    todo!()
+}
 
+struct Board {
+    grid: Array3<Hex>,
+}
+
+#[derive(Clone, Default)]
 struct Hex {
     size: i32,
     in_radius: i32,
